4df2c5aa6224211682fef43f49cd4a6e1151c15f
2pk03 <alo.alt@me.com>
2026-03-18 10:57:41 +0100

    Harden commit-check deps and ignore generated lint artifacts

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', '.tmp', 'coverage', 'node_modules']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
